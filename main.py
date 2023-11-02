695962d4733f83cd31c44e215d4027a0f4b0be4c
Aayushman777 <94427975+Aayushman777@users.noreply.github.com>
2023-11-02 15:24:25 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-import pygame, sys
+import pygame, sys, random
 from object import *
 from frog import *
+from lane import *
+from pygame.locals import*
+
 
 # main game
 class Game:
     def __init__(self, screen_dimensions, screen_caption, screen_color):
         pygame.init()
+        pygame.joystick.init()
         self.clock = pygame.time.Clock()
         pygame.display.set_mode(screen_dimensions)
         pygame.display.set_caption(screen_caption)
@@ -19,34 +23,53 @@ class Game:
         self.frog_group = pygame.sprite.Group() # grouping frog variations
 
         self.all_groups = [self.object_group, self.car_group, self.river_group, self.frog_group]
-
+        self.river_speeds = {} # river lane speeds dictionary
         self.assetSetup()
 
-    # display assets
+    # display and configure assets
     def assetSetup(self):
-        Object((0,0), (672,768), "assets/background.png", self.object_group)
+        Object((0,0), (672,768), "assets/background.png", self.object_group) # background
         
         for x in range(14):
             Object((x*48, 384), (48,48), "assets/grass/purple.png", self.object_group) # purple grass dividing river and road
             Object((x*48, 672), (48,48), "assets/grass/purple.png", self.object_group) # purple grass stretching along starting row
         for x in range(28):
-            Object((x*24, 72), (24, 72), "assets/grass/green.png", self.object_group)
+            Object((x*24, 72), (24, 72), "assets/grass/green.png", self.object_group) # green grass at finish
 
-        self.frog = Frog((336,672), (48,48), "assets/frog/frog_up.png", self.frog_group)
+        # lanes
+        speeds = [-1.25, -1, -.75, -.5, -.25, .25, .5, .75, 1, 1.25] # speeds for objects moving across lanes (negatives move left and positives move right)
+        random.shuffle(speeds)
 
+        # river lanes
+        for y in range(5):
+            y_pos = y*48 + 144 # going from top river lane to bottom
+            new_lane = Lane((0, y_pos), self.river_group, speeds.pop(), "river") # set up each lane and add a speed from the speed list
+            self.river_speeds[y_pos//48] = new_lane.speed # keys for river lane dictionary (3, 4, 5, 6, 7(corresponding rows))
+
+        # street lanes
+        for y in range(5):
+            y_pos = y*48 + 432 # going from top street lane to bottom
+            Lane((0, y_pos), self.car_group, speeds.pop(), "street") # set up each lane and add a speed from the speed list
+
+        self.frog = Frog((336,672), (48,48), "assets/frog/frog_up.png", self.frog_group, [self.car_group, self.river_group], self.river_speeds) # player frog
 
     # main game loop
     def run(self):
         while True:
             self.clock.tick(60) # frame rate
-
-            self.frog.keyups = []          
+            self.joysticks = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
+            self.frog.keyups = []  # store pressed keys        
             for event in pygame.event.get():                
                 if event.type == pygame.QUIT:   # close the game when quit
                     pygame.quit()
                     sys.exit()
-                elif event.type == pygame.KEYUP:
+                elif event.type == pygame.KEYUP:  # store pressed keys
                     self.frog.keyups.append(event.key)
+                # Handle joystick events
+                if event.type == pygame.JOYBUTTONDOWN or event.type == pygame.JOYBUTTONUP:
+                    self.frog.handleJoystickEvent(event)
+                elif event.type == JOYHATMOTION:
+                    print(event)
 
             # update sprites
             for group in self.all_groups:
